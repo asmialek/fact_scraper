606db4309e3239ef408a70abd9e14654c9024e4f
asmialek <asmialek@cbk.waw.pl>
2018-06-04 12:34:42 +0100

    Add another page

--- scrape.py
+++ scrape.py
@@ -12,9 +12,10 @@ def find_facts(page_link):
     # print(page_content.prettify())
     for ol in page_content.select('ol'):
         for li in ol.select('li'):
-            globals()['i'] += 1
-            print(globals()['i'], li.text)
-            pass
+            if 'adsbygoogle' not in li.text:
+                globals()['i'] += 1
+                print(globals()['i'], li.text)
+                pass
 
     next_page_link = page_content.find('div',
                                            attrs={'class': 'page-links2'})
@@ -25,6 +26,7 @@ def find_facts(page_link):
 
 
 page_link = 'https://www.thefactsite.com/2010/09/300-random-animal-facts.html'
+page_link = 'https://www.thefactsite.com/2012/01/100-random-facts-about-space.html'
 
 headers = {'User-Agent': generate_user_agent(device_type="desktop",
                                              os=('mac', 'linux'))}
